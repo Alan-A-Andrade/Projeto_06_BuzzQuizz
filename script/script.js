d864cb1d7d6b10e6684ddc2ace111b0bbefcb411
Alan <alanaa92@gmail.com>
2021-11-18 20:04:05 -0300

    Teste de Mesclagem de codigo no github

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@
 
 function getQuizzByID(element) {
 
+    let pageContainer = document.querySelector(".container")
+
 
     ///// em alguma parte do elemento terá informação do quizz que a pessoa está clicando
     //// essa informação ID unico do quizz
@@ -12,12 +14,50 @@ function getQuizzByID(element) {
     askPromisse.catch(promisseFail);
 
     function promisseFulfilled(answer) {
-        console.log(answer.data);
+
+        showQuizzQuestions(answer)
+
 
     }
 
     function promisseFail(answer) {
         console.log(answer);
+
+    }
+
+    function showQuizzQuestions(quizzData) {
+
+        pageContainer.innerHTML = ""
+        console.log(quizzData.data.questions);
+
+
+        pageContainer.innerHTML += `
+        <section class="banner-quizz">
+        <h1>${quizzData.data.title}</h1>
+        <img src="${quizzData.data.image}" alt=""/>
+        </section>
+
+        <div class="question-card">
+        <div class="answer-card">
+        <img src="https://picsum.photos/600/400" alt=""/>
+        <h1>Resposta Aqui</h1>
+        </div>
+        <div class="answer-card">
+        <img src="https://picsum.photos/600/400" alt=""/>
+        <h1>Resposta Aqui</h1>
+        </div>
+        <div class="answer-card">
+        <img src="https://picsum.photos/600/400" alt=""/>
+        <h1>Resposta Aqui</h1>
+        </div>
+        <div class="answer-card">
+        <img src="https://picsum.photos/600/400" alt=""/>
+        <h1>Resposta Aqui</h1>
+        </div>
+        </div>
+
+
+        `
 
     }
 }
