2b592ede92776b8e0c959edb952f00e14f266a77
Rui Neto <ruineto11@gmail.com>
2021-11-19 02:49:35 -0300

    Projeto #06 BuzzQuizz - Botão de navegação finalizado

--- script/script.js
+++ script/script.js
@@ -1,100 +1,124 @@
 let listQuizzesInterval = setInterval(listQuizzesRequest, 1000);
 function listQuizzesRequest() {
-    promisseGetQuizzes = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
-    promisseGetQuizzes.then(listQuizzes);
+  promisseGetQuizzes = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+  promisseGetQuizzes.then(listQuizzes);
 }
 
 function listQuizzes(answerListQuizzes) {
-    let quizzes = answerListQuizzes.data;
-    let quizzesListed = document.querySelector(".all-quizzes-listed");
+  let quizzes = answerListQuizzes.data;
+  let quizzesListed = document.querySelector(".all-quizzes-listed");
 
-    quizzesListed.innerHTML = "";
-    for (let i = 0; i < quizzes.length; i++) {
-        let quizz = quizzes[i];
+  quizzesListed.innerHTML = "";
+  for (let i = 0; i < quizzes.length; i++) {
+    let quizz = quizzes[i];
 
-        quizzesListed.innerHTML += `
+    quizzesListed.innerHTML += `
     <div class="all-quizzes-quizz" onclick="getQuizzByID(${quizz.id})">
       <!--all-quizzes-quizz-bg-->
       <img class="all-quizzes-quizz-bg" src="${quizz.image}">
+
       <!--all-quizzes-quizz-degrade-->
       <div class="all-quizzes-quizz-degrade"></div>
+
       <!--all-quizzes-quizz-title-->
       <div class="all-quizzes-quizz-title">
         <p>${quizz.title}</p>
       </div>
     </div><!--all-quizzes-quizz-->        
     `;
-    }
+  }
 }
 
+function createQuizz(){
+  let pageContainer = document.querySelector(".container");
+  pageContainer.innerHTML = "";
+
+  pageContainer.innerHTML = `
+  <section class="create-quizzes">
+    <!--create-quizzes-title-->
+    <h2 class="create-quizzes-title">Comece pelo começo</h2>
+
+    <!--create-quizzes-inputs-->
+    <div class="create-quizzes-inputs">
+      <!--create-quizzes-input-->
+      <input class="create-quizzes-input" placeholder="Título do seu quizz">
+
+      <!--create-quizzes-input-->
+      <input class="create-quizzes-input" placeholder="URL da imagem do seu quizz">
+
+      <!--create-quizzes-input-->      
+      <input class="create-quizzes-input" placeholder="Quantidade de perguntas do quizz">
+
+      <!--create-quizzes-input-->
+      <input class="create-quizzes-input" placeholder="Quantidade de níveis do quizz">
+    </div>
+
+    <!--create-quizzes-button-->
+    <button class="create-quizzes-button" onclick="createQuizzQuestions()">Prosseguir pra criar perguntas</button>
+  <section><!--create-quizzes-->
+  `;
+}
+
+function createQuizzQuestions(){
+  alert("teste");
+}
+
+
 function getQuizzByID(element) {
+  clearInterval(listQuizzesInterval);
+  let pageContainer = document.querySelector(".container");
 
-    clearInterval(listQuizzesInterval);
+  askPromisse = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${element}`);
+  askPromisse.then(promisseFulfilled);
+  askPromisse.catch(promisseFail);
 
-    let pageContainer = document.querySelector(".container")
+  function promisseFulfilled(answer) {
+    showQuizzQuestions(answer);
+  }
 
-    askPromisse = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${element}`);
-    askPromisse.then(promisseFulfilled);
-    askPromisse.catch(promisseFail);
+  function promisseFail(answer) {
+    console.log(answer);
+  }
 
-    function promisseFulfilled(answer) {
+  function showQuizzQuestions(quizzData) {
+    pageContainer.innerHTML = "";
 
-        showQuizzQuestions(answer)
+    //Add banner
+    pageContainer.innerHTML += `
+    <div class="quizz-conteiner">
+      <section class="banner-quizz">
+      <h1>${quizzData.data.title}</h1>
+      <img src="${quizzData.data.image}" alt=""/>
+      </section>
+    </div>
+    `;
 
-    }
-
-    function promisseFail(answer) {
-        console.log(answer);
-
-    }
-
-    function showQuizzQuestions(quizzData) {
-
-        pageContainer.innerHTML = ""
-
-        //add banner
-        pageContainer.innerHTML += `
-        <div class="quizz-conteiner">
-        <section class="banner-quizz">
-        <h1>${quizzData.data.title}</h1>
-        <img src="${quizzData.data.image}" alt=""/>
-        </section>
-        </div>`
-
-        //add N questions cards
-        for (let i = 0; i < quizzData.data.questions.length; i++) {
-
-            pageContainer.querySelector(".quizz-conteiner").innerHTML +=
-                `     <div class="question-card" data-identifier="question">
+    //Add N questions cards
+    for (let i = 0; i < quizzData.data.questions.length; i++) {
+      pageContainer.querySelector(".quizz-conteiner").innerHTML += `     
+      <div class="question-card" data-identifier="question">
         <div style="background-color: ${quizzData.data.questions[i].color}" class="question-title">
         <h1>${quizzData.data.questions[i].title}</h1>
-        </div>`
+      </div>
+      `;
 
-            //add M answers to N card
+      //Add M answers to N card
+      let arrayNumbAnswer = [];
+      for (let w = 0; w < quizzData.data.questions[i].answers.length; w++) {
+        arrayNumbAnswer.push(w);
+      }
 
-            let arrayNumbAnswer = [];
-
-            for (let w = 0; w < quizzData.data.questions[i].answers.length; w++) {
-                arrayNumbAnswer.push(w);
-            }
-
-            arrayNumbAnswer.sort(() => Math.random() - 0.5);
-
-            for (let j = 0; j < quizzData.data.questions[i].answers.length; j++) {
-
-
-
-                pageContainer.querySelector(".question-card:last-child").innerHTML +=
-                    `
-                <div class="answer-card ${quizzData.data.questions[i].answers[arrayNumbAnswer[j]].isCorrectAnswer}" data-identifier="answer">
-                <div class="answer-img-container">
-                <img src="${quizzData.data.questions[i].answers[arrayNumbAnswer[j]].image}" alt=""/>
-                </div>
-                <h1>${quizzData.data.questions[i].answers[arrayNumbAnswer[j]].text}</h1>
-                </div>`
-
-
-            }
-        }
+      arrayNumbAnswer.sort(() => Math.random() - 0.5);
+      for (let j = 0; j < quizzData.data.questions[i].answers.length; j++) {
+        pageContainer.querySelector(".question-card:last-child").innerHTML += `
+        <div class="answer-card ${quizzData.data.questions[i].answers[arrayNumbAnswer[j]].isCorrectAnswer}" data-identifier="answer">
+          <div class="answer-img-container">
+            <img src="${quizzData.data.questions[i].answers[arrayNumbAnswer[j]].image}" alt=""/>
+          </div>
+          <h1>${quizzData.data.questions[i].answers[arrayNumbAnswer[j]].text}</h1>
+        </div>
+        `;
+      }
     }
+  }
 }
