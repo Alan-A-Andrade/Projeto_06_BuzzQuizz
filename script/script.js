ff4664854378d760406ebb518579f6d0c7b1bfa0
Alan <alanaa92@gmail.com>
2021-11-18 22:35:29 -0300

    Funcionalidade Exibir Quizz (sem pergunta embaralhada)

--- script/script.js
+++ script/script.js
@@ -28,36 +28,80 @@ function getQuizzByID(element) {
     function showQuizzQuestions(quizzData) {
 
         pageContainer.innerHTML = ""
+
+
+        let i = 0;
+
+        let j = 0;
+        let questionData = quizzData.data.questions[0]
+        let answerData = questionData.answer
+        console.log(quizzData.data)
         console.log(quizzData.data.questions);
+        console.log(quizzData.data.questions[i]);
+        console.log(quizzData.data.questions[i].title);
+        console.log(quizzData.data.questions[i].answers);
+        console.log(quizzData.data.questions[i].answers[j]); //embaralhar J
+        console.log(quizzData.data.questions[i].answers[j].image)
+        console.log(quizzData.data.questions[i].answers[j].text)
+        console.log(quizzData.data.questions[i].answers[j].isCorrectAnswer)
+        console.log(quizzData.data.questions[i].answers.length)
+        //for (let x< 0)
 
 
+        //add banner
         pageContainer.innerHTML += `
+        <div class="quizz-conteiner">
         <section class="banner-quizz">
         <h1>${quizzData.data.title}</h1>
         <img src="${quizzData.data.image}" alt=""/>
         </section>
+        </div>`
 
+        //add N questions cards
+        for (let i = 0; i < quizzData.data.questions.length; i++) {
+
+            pageContainer.querySelector(".quizz-conteiner").innerHTML +=
+                `     <div class="question-card">
+        <div style="background-color: ${quizzData.data.questions[i].color}" class="question-title">
+        <h1>${quizzData.data.questions[i].title}</h1>
+        </div>`
+
+            for (let j = 0; j < quizzData.data.questions[i].answers.length; j++) {
+                pageContainer.querySelector(".question-card:last-child").innerHTML +=
+                    `
+                <div class="answer-card ${quizzData.data.questions[i].answers[j].isCorrectAnswer}">
+                <div class="answer-img-container">
+                <img src="${quizzData.data.questions[i].answers[j].image}" alt=""/>
+                </div>
+                <h1>${quizzData.data.questions[i].answers[j].text}</h1>
+                </div>`
+
+
+            }
+
+        }
+
+        //add N questions to M Question Card
+
+
+        /*
         <div class="question-card">
-        <div class="answer-card">
-        <img src="https://picsum.photos/600/400" alt=""/>
-        <h1>Resposta Aqui</h1>
+        <div style="background-color: ${quizzData.data.questions[0].color}" class="question-title">
+        <h1>${quizzData.data.questions[i].title}</h1>
         </div>
-        <div class="answer-card">
-        <img src="https://picsum.photos/600/400" alt=""/>
-        <h1>Resposta Aqui</h1>
+ 
+        <div class="answer-card ${quizzData.data.questions[i].answers[j].isCorrectAnswer}">
+        <div class="answer-img-container">
+        <img src="${quizzData.data.questions[i].answers[j].image}" alt=""/>
         </div>
-        <div class="answer-card">
-        <img src="https://picsum.photos/600/400" alt=""/>
-        <h1>Resposta Aqui</h1>
+        <h1>${quizzData.data.questions[i].answers[j].text}</h1>
         </div>
-        <div class="answer-card">
-        <img src="https://picsum.photos/600/400" alt=""/>
-        <h1>Resposta Aqui</h1>
+    
+         </div>
         </div>
-        </div>
+        */
 
 
-        `
 
     }
 }
