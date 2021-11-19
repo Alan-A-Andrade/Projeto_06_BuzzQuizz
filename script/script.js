5a83f2110679f2b2c31f21365056b3a5fdac4ab5
Alan <alanaa92@gmail.com>
2021-11-18 23:02:20 -0300

    implementação exigir quizz (sem embaralhar respostas)

--- script/script.js
+++ script/script.js
@@ -1,13 +1,6 @@
-
-
-
 function getQuizzByID(element) {
 
     let pageContainer = document.querySelector(".container")
-
-
-    ///// em alguma parte do elemento terá informação do quizz que a pessoa está clicando
-    //// essa informação ID unico do quizz
 
     askPromisse = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${element}`);
     askPromisse.then(promisseFulfilled);
@@ -28,25 +21,6 @@ function getQuizzByID(element) {
     function showQuizzQuestions(quizzData) {
 
         pageContainer.innerHTML = ""
-
-
-        let i = 0;
-
-        let j = 0;
-        let questionData = quizzData.data.questions[0]
-        let answerData = questionData.answer
-        console.log(quizzData.data)
-        console.log(quizzData.data.questions);
-        console.log(quizzData.data.questions[i]);
-        console.log(quizzData.data.questions[i].title);
-        console.log(quizzData.data.questions[i].answers);
-        console.log(quizzData.data.questions[i].answers[j]); //embaralhar J
-        console.log(quizzData.data.questions[i].answers[j].image)
-        console.log(quizzData.data.questions[i].answers[j].text)
-        console.log(quizzData.data.questions[i].answers[j].isCorrectAnswer)
-        console.log(quizzData.data.questions[i].answers.length)
-        //for (let x< 0)
-
 
         //add banner
         pageContainer.innerHTML += `
@@ -80,28 +54,5 @@ function getQuizzByID(element) {
             }
 
         }
-
-        //add N questions to M Question Card
-
-
-        /*
-        <div class="question-card">
-        <div style="background-color: ${quizzData.data.questions[0].color}" class="question-title">
-        <h1>${quizzData.data.questions[i].title}</h1>
-        </div>
- 
-        <div class="answer-card ${quizzData.data.questions[i].answers[j].isCorrectAnswer}">
-        <div class="answer-img-container">
-        <img src="${quizzData.data.questions[i].answers[j].image}" alt=""/>
-        </div>
-        <h1>${quizzData.data.questions[i].answers[j].text}</h1>
-        </div>
-    
-         </div>
-        </div>
-        */
-
-
-
     }
 }
